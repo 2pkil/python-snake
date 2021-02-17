8bcad1dd22b4709b87840846c9d21e5033166e28
yodo-im <78910536+yodo-im@users.noreply.github.com>
2021-02-17 13:39:03 +0300

    Update eat_snake.py

--- eat_snake.py
+++ eat_snake.py
@@ -1,4 +1,4 @@
-    if snake[-1] == apple:
-        apple = randrange(0, WIN, SIZE), randrange(0, WIN, SIZE)
-        length = length + 1
-        fps = fps + 1
+if snake[-1] == apple:
+    apple = randrange(0, WIN, SIZE), randrange(0, WIN, SIZE)
+    length = length + 1
+    fps = fps + 1
