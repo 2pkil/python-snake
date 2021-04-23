efb79879c364e6fbe20078cfbb4e9befee4cedf7
yodo-im <78910536+yodo-im@users.noreply.github.com>
2021-04-23 16:24:34 +0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,13 +1,9 @@
-key = pygame.key.get_pressed()
-if key[pygame.K_w] and dirs ['W']:
-    dx, dy = 0, -1
-    dirs = {'W': True, 'S': False, 'A': True, 'D': True}
-if key[pygame.K_s] and dirs ['S']:
-    dx, dy = 0, 1
-    dirs = {'W': False, 'S': True, 'A': True, 'D': True}
-if key[pygame.K_a] and dirs ['A']:
-    dx, dy = -1, 0
-    dirs = {'W': True, 'S': True, 'A': True, 'D': False}
-if key[pygame.K_d] and dirs ['D']:
-    dx, dy = 1, 0
-    dirs = {'W': True, 'S': True, 'A': False, 'D': True}
+# Условия движения
+if key[pygame.K_w] and (dx, dy) != traffic_dict["S"]:
+    dx, dy = traffic_dict["W"]
+if key[pygame.K_s] and (dx, dy) != traffic_dict["W"]:
+    dx, dy = traffic_dict["S"]
+if key[pygame.K_a] and (dx, dy) != traffic_dict["D"]:
+    dx, dy = traffic_dict["A"]
+if key[pygame.K_d] and (dx, dy) != traffic_dict["A"]:
+    dx, dy = traffic_dict["D"]
